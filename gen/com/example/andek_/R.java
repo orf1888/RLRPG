84f707aee212196f715501721a7613047ea0b83b
Serhii Kilinich <orf1888@gmail.com>
2014-04-22 13:49:48 +0200

    upgrade achiv xml

--- gen/com/example/andek_/R.java
+++ gen/com/example/andek_/R.java
@@ -25,20 +25,19 @@ public final class R {
         public static final int stick=0x7f020001;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080006;
-        public static final int achivDesription=0x7f080003;
+        public static final int LinearLayout1=0x7f080005;
+        public static final int achivDesription=0x7f080002;
         public static final int achivImg=0x7f080000;
         public static final int achivName=0x7f080001;
-        public static final int achivStart=0x7f080004;
-        public static final int action_settings=0x7f08000d;
-        public static final int btnAchive=0x7f08000b;
-        public static final int btnCom=0x7f08000c;
-        public static final int list=0x7f080005;
-        public static final int myAcc=0x7f080007;
-        public static final int tableRow=0x7f080002;
-        public static final int textLblLvl=0x7f080009;
-        public static final int textLblNick=0x7f080008;
-        public static final int textLblXp=0x7f08000a;
+        public static final int achivStart=0x7f080003;
+        public static final int action_settings=0x7f08000c;
+        public static final int btnAchive=0x7f08000a;
+        public static final int btnCom=0x7f08000b;
+        public static final int list=0x7f080004;
+        public static final int myAcc=0x7f080006;
+        public static final int textLblLvl=0x7f080008;
+        public static final int textLblNick=0x7f080007;
+        public static final int textLblXp=0x7f080009;
     }
     public static final class layout {
         public static final int achiv_item=0x7f030000;
