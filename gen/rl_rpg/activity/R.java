3aa03e51b5c5c6e46cac19098359688f5f4ca6f1
Serhii Kilinich <orf1888@gmail.com>
2014-04-22 21:38:25 +0200

    start implement MyAccountActivity

--- gen/rl_rpg/activity/R.java
+++ gen/rl_rpg/activity/R.java
@@ -26,33 +26,42 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f080011;
         public static final int btnChallenge=0x7f080005;
         public static final int btnCom=0x7f080006;
-        public static final int challengeDesription=0x7f080009;
-        public static final int challengeImg=0x7f080007;
-        public static final int challengeName=0x7f080008;
-        public static final int challengeStart=0x7f08000a;
-        public static final int list=0x7f08000b;
+        public static final int challengeDesription=0x7f08000e;
+        public static final int challengeImg=0x7f08000c;
+        public static final int challengeName=0x7f08000d;
+        public static final int challengeStart=0x7f08000f;
+        public static final int list=0x7f080010;
         public static final int myAcc=0x7f080001;
+        public static final int progressBar1=0x7f080009;
+        public static final int progressBar2=0x7f08000a;
+        public static final int progressBar3=0x7f08000b;
         public static final int textLblLvl=0x7f080003;
         public static final int textLblNick=0x7f080002;
         public static final int textLblXp=0x7f080004;
+        public static final int textView1=0x7f080007;
+        public static final int textView2=0x7f080008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int challenge_item=0x7f030001;
-        public static final int challenge_list=0x7f030002;
+        public static final int activity_my_account=0x7f030001;
+        public static final int challenge_item=0x7f030002;
+        public static final int challenge_list=0x7f030003;
     }
     public static final class menu {
         public static final int achivment=0x7f070000;
         public static final int main=0x7f070001;
+        public static final int my_account=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_description=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050005;
         public static final int title_activity_achivment=0x7f050003;
+        public static final int title_activity_my_account=0x7f050004;
     }
     public static final class style {
         /** 
